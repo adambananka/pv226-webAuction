57652b1bde3ccfc8df899b78e56f621fad11c770
Adam <bananka.adam@gmail.com>
2017-11-24 20:17:00 +0100

    auctionList stuff added

---
+++ WebAuction.PresentationLayer/Models/Auctions/AuctionListViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using WebAuction.BusinessLayer.DataTransferObjects;
+using WebAuction.BusinessLayer.DataTransferObjects.Filters;
+using X.PagedList;
+
+namespace WebAuction.PresentationLayer.Models.Auctions
+{
+    public class AuctionListViewModel
+    {
+        public string[] AuctionSortCriteria => new[] { nameof(AuctionDto.Name), nameof(AuctionDto.ActualPrice), nameof(AuctionDto.EndTime) };
+
+        public IList<CategoryDto> Categories { get; set; }
+
+        public IPagedList<AuctionDto> Auctions { get; set; }
+
+        public AuctionFilterDto Filter { get; set; }
+
+        public SelectList AllSortCriteria => new SelectList(AuctionSortCriteria);
+    }
+}
