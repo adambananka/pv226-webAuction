e604462a0848f0d49d9759e126ad8b5640476f9f
Stefan Svinciak <stefan.svinciak@gmail.com>
2017-11-24 15:37:38 +0100

    auctioncontroller created

---
+++ WebAuction.PresentationLayer/Controllers/AuctionsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using WebAuction.BusinessLayer.Facades;
+
+namespace WebAuction.PresentationLayer.Controllers
+{
+    public class AuctionsController : Controller
+    {
+        public AuctionProcessFacade AuctionProcessFacade { get; set; }
+
+        public async Task<ActionResult> Details(Guid id)
+        {
+            var model = await AuctionProcessFacade.GetAuctionAsync(id);
+            throw new NotImplementedException("trololo");
+        }
+    }
+}
