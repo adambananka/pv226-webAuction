823296f00c7605765df6d849a2c968ea72abcc61
Stefan Svinciak <stefan.svinciak@gmail.com>
2017-11-24 20:39:03 +0100

    DI container added

--- WebAuction.PresentationLayer/Global.asax.cs
+++ WebAuction.PresentationLayer/Global.asax.cs
@@ -1,21 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Castle.Windsor;
+using WebAuction.BusinessLayer.Config;
+using WebAuction.PresentationLayer.Windsor;
 
 namespace WebAuction.PresentationLayer
 {
-    public class MvcApplication : System.Web.HttpApplication
+    public class MvcApplication : HttpApplication
     {
+        private static readonly IWindsorContainer Container = new WindsorContainer();
+
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            BootstrapContainer();
+        }
+
+        private void BootstrapContainer()
+        {
+            Container.Install(new BusinessLayerInstaller());
+            Container.Install(new PresentationLayerInstaller());
+
+            var controllerFactory = new WindsorControllerFactory(Container.Kernel);
+            ControllerBuilder.Current.SetControllerFactory(controllerFactory);
         }
     }
 }
